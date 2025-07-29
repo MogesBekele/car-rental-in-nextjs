19bd280e7929b4fa797f429fdd86f46cce65fdac
MogesBekele <mogesbekele32@gmail.com>
2025-07-29 13:02:39 +0300

    setup all dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center>">
       <h1 className="text-4xl font-bold text-center">Car Rental</h1>
-    
     </div>
   );
 }
