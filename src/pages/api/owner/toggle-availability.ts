7592bb026ac8966746796cbcf867f91503468644
MogesBekele <mogesbekele32@gmail.com>
2025-10-17 12:04:50 +0300

    toggle car availability

--- src/pages/api/owner/toggle-availability.ts
+++ src/pages/api/owner/toggle-availability.ts
@@ -1,9 +1,9 @@
 // src/pages/api/owner/toggle-availability.ts
 
-import type { NextApiResponse } from 'next';
-import type { NextApiRequestWithUser } from '@/types/nextApiRequestWithUser';
-import { protect } from '@/lib/auth';
-import Car from '@/models/Car';
+import type { NextApiResponse } from "next";
+import type { NextApiRequestWithUser } from "@/types/nextApiRequestWithUser";
+import { protect } from "@/lib/auth";
+import Car from "@/models/Car";
 
 interface ApiResponse {
   success: boolean;
@@ -15,7 +15,7 @@ const handler = async (
   req: NextApiRequestWithUser,
   res: NextApiResponse<ApiResponse>
 ) => {
-  if (req.method !== 'POST') {
+  if (req.method !== "POST") {
     return res
       .status(405)
       .json({ success: false, message: `Method ${req.method} Not Allowed` });
@@ -28,25 +28,24 @@ const handler = async (
     const car = await Car.findById(carId);
 
     if (!car) {
-      return res.status(404).json({ success: false, message: 'Car not found' });
+      return res.status(404).json({ success: false, message: "Car not found" });
     }
 
     if (car.owner.toString() !== _id.toString()) {
-      return res.status(401).json({ success: false, message: 'Unauthorized' });
+      return res.status(401).json({ success: false, message: "Unauthorized" });
     }
 
     car.isAvailable = !car.isAvailable;
     await car.save();
-
     res.status(200).json({
       success: true,
-      message: 'Car availability toggled successfully',
+      message: "Car availability toggled successfully",
     });
   } catch (error: unknown) {
     console.error(error);
     res.status(500).json({
       success: false,
-      message: 'Server error',
+      message: "Server error",
     });
   }
 };
